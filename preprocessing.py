db7a706eb878b16c4bff7611edb06970c0f3bae6
gargipandkar <54140892+gargipandkar@users.noreply.github.com>
2022-07-28 23:00:18 +0800

    Add pseudo labelling

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import contractions
+import re
 
 stop_words = set(stopwords.words('english'))
 punctuations = '''!()-[]{};:’'"\,<>./?@#$%^&*_~'''
@@ -11,10 +12,10 @@ def preprocess_word(word):
     word = word.lower()
     word = lemmatizer.lemmatize(word)
     word = contractions.fix(word)
+    word =  re.sub('\W+','', word)
     return word
 
-def preprocess_text(text):
-    text = text.replace('—', ' ')
+def preprocess_text(text): 
     word_tokens = word_tokenize(text)
     filtered = [preprocess_word(w) for w in word_tokens if w.lower() not in stop_words and w not in punctuations]
     return filtered
