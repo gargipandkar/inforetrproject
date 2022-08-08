b5d19547a552b83d35f446f557e4d64ce6540b7a
gargipandkar <54140892+gargipandkar@users.noreply.github.com>
2022-08-08 12:03:57 +0800

    Try pretrained DPR

---
+++ dpr.py
@@ -0,0 +1,34 @@
+'''
+Using pretrained Dense Passage Retrieval encoders
+'''
+
+from sentence_transformers import SentenceTransformer, util
+import torch 
+
+import numpy as np 
+import pandas as pd
+
+if __name__=="__main__":
+   
+    doc_encoder = SentenceTransformer('facebook-dpr-ctx_encoder-single-nq-base')
+    query_encoder = SentenceTransformer('facebook-dpr-question_encoder-single-nq-base')
+     
+    # docs = [
+    #     "London [SEP] London is the capital and largest city of England and the United Kingdom.",
+    #     "Paris [SEP] Paris is the capital and most populous city of France.",
+    #     "Berlin [SEP] Berlin is the capital and largest city of Germany by both area and population."
+    # ]
+    
+    # query = "What is the capital of England?"
+    
+    docs = pd.read_csv('./data/documents.csv')[:20]
+    doc_embeddings = doc_encoder.encode(docs['data'])
+
+    query = "Where can I find good vegetarian food?"
+    query_embedding = query_encoder.encode(query)
+
+    scores = util.dot_score(query_embedding, doc_embeddings)
+    _, topk = torch.topk(scores, k=3, sorted=True)
+    topk = topk.flatten().tolist()
+    print("Query:", query)
+    print("Docs:", [docs.iloc[i]['filename'] for i in topk])
